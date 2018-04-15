9f14ff5a9134a5c2d599b0d332661e0beaf3c9be
MargotDem <margot_demaulmont@hotmail.fr>
2018-04-15 17:20:00 +0200

    display real statistics on google map

--- app/Statistique.php
+++ app/Statistique.php
@@ -9,7 +9,7 @@ class Statistique extends Model
 {
   public static function index()
   {
-    $statistics = DB::select('SELECT id, academie, moyenne from statistiques ORDER BY moyenne DESC');
+    $statistics = DB::select('SELECT * from statistiques ORDER BY moyenne DESC');
     return $statistics;
   }
 }
