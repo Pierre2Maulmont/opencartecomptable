15e1fd68ed8cd7e35d3ba3db93431f7f748254ba
MargotDem <margot_demaulmont@hotmail.fr>
2018-05-14 15:59:06 +0200

    test requests

--- resources/assets/js/containers/schools/Schools.js
+++ resources/assets/js/containers/schools/Schools.js
@@ -24,7 +24,7 @@ export default class Schools extends Component {
       // fetch schools corresponding to search criteria
       let isAgencies = pathname === '/agences' ? '&agencies' : ''
       let url = this.props.location
-      const requestUrl = 'http://opencartecomptable.herokuapp.com/public/api/etablissements' + url.search + isAgencies
+      const requestUrl = 'https://opencartecomptable.herokuapp.com/api/etablissements' + url.search + isAgencies
       axios.get(requestUrl)
         .then(schools => {
           this.setState({ schools: schools.data })
@@ -35,7 +35,7 @@ export default class Schools extends Component {
     } else {
       // fetch corresponding school
       let url = this.props.location
-      const requestUrl = 'http://opencartecomptable.herokuapp.com/public/api' + url.pathname
+      const requestUrl = 'https://opencartecomptable.herokuapp.com/public/api' + url.pathname
       axios.get(requestUrl)
         .then(school => {
           this.setState({ schools: school.data })
